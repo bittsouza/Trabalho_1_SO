5bff89783a1c19efee206868e7caf5db36f8dc97
Gabriel Bittencourt de Souza <gabasbds@hotmail.com>
2018-09-28 22:53:48 -0300

    Versao servidor printando informacoes e systemls

--- main.c
+++ main.c
@@ -8,24 +8,89 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h>
+#include <pthread.h>
+#include <dirent.h>
 
-int main()
+
+pthread_mutex_t mutex; // precisa ser global!
+
+char* listDir(){
+	char *retorno = calloc(2048,1);
+	int offset = 0;
+
+	DIR *d;
+    struct dirent *dir;
+    d = opendir(".");
+
+    if (d)
+    {
+        while ((dir = readdir(d)) != NULL)
+        {
+            offset = offset + sprintf(retorno + offset, "%s ", dir->d_name);
+        }
+        closedir(d);
+    }
+
+    return retorno;
+}
+				
+void *func_thread(void *arg) {
+
+	pthread_mutex_lock(&mutex);
+	//região crı́tica
+	pthread_mutex_unlock(&mutex);
+	return 0;
+}
+
+void *print(void *threadid)
+{
+   int *connfd = (int *)threadid;
+   char sendBuff[1025];
+
+   
+   printf("Socket na thread %d\n", *connfd);
+
+
+   int recebido = read(*connfd, sendBuff, 1025);
+   printf ("Valor recebido foi: %s", sendBuff);
+   
+   char msg[100];
+   sprintf(msg, "Recebi o valor %d bytes\n", recebido);
+
+   system("ls meuarquivo.txt > saida.txt");
+
+   char* ls = listDir();
+
+
+   write(*connfd, ls, 2048);
+   free(ls);
+   printf("Thread terminado\n");
+   close(*connfd); 	
+
+
+   free(connfd);
+   pthread_exit(NULL);
+};
+
+
+int main(int argc, char **argv)
 {
 
-		int listenfd = 0, connfd = 0;
+		int listenfd = 0;
 		int message = 0;
 		struct sockaddr_in serv_addr;
 
-		char sendBuff[1025];
-		time_t ticks;
+		
+		//time_t ticks;
+
 
 		listenfd = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP);
 		memset(&serv_addr, '0', sizeof(serv_addr));
-		memset(sendBuff, '0', sizeof(sendBuff));
+		
 
 		serv_addr.sin_family = AF_INET;
 		serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-		serv_addr.sin_port = htons(5000);
+		serv_addr.sin_port = htons(5001);
 
 		bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
@@ -34,27 +99,35 @@ int main()
 
 		while(1)
 		{
-			connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
 
-				printf("Conexão realizada!\n");
+			int *connfd = calloc(sizeof(int),1);
+			
+			pthread_t thread;
 
-			ticks = time(NULL);
+
+			*connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+			
+
+				printf("Socket criado: %d\n", *connfd);
+
+				pthread_mutex_init(&mutex, NULL);
+
+				pthread_create(&thread, NULL, print, connfd);
 			
             
-            snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+//            snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
  //			write(connfd, sendBuff, strlen(sendBuff));
 
 			//bzero(sendBuff, sizeof(sendBuff));
 
 
-            int bytesLidos = read(connfd, sendBuff, strlen(sendBuff));
             
-            for(int i = 0 ; i < (bytesLidos) ; i++)
-            	printf ("%c", sendBuff[i]);
             
 
-			close(connfd);
-			sleep(1);
+			//close(connfd);
+			//pthread_join(thread, NULL);
+			//pthread_mutex_destroy(&mutex);
+			//sleep(1);
 		}
 
 }
