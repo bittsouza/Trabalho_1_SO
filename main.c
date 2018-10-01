d6e3de387ff370fa36ebed1c62fbb0e57acc1ed1
Gabriel Bittencourt de Souza <gabasbds@hotmail.com>
2018-10-01 11:21:22 -0300

    comandos funcionando n cd

--- main.c
+++ main.c
@@ -1,3 +1,29 @@
+/*Desenvolver um sistema de arquivos distribuídos que simula padrão EXT3. 
+O sistema deve ser desenvolvido em C ou C++ utilizando o compilador GNU GCC e chamadas de sistemas do padrão POSIX. 
+O sistema deve permitir que arquivos locais sejam acessados por usuários remotos simultaneamente. 
+As operações permitidas pelo sistema devem incluir:
+	
+	criar (sub)diretório (mkdir)
+	remover (sub)diretório (rm -r)
+	entrar em (sub)diretório (cd)
+	mostrar conteúdo do diretório (ls)
+	criar arquivo (touch)
+	remover arquivo (rm)
+	escrever um sequência de caracteres em um arquivo (echo)
+	mostrar conteúdo do arquivo (cat)
+
+Etapa 1
+Desenvolver a estrutura de acesso do servidor de arquivos. Ele deverá será acessado via socket TCP.
+Cada conexão deverá ser gerida por uma thread. Condições de corrida deverão ser tratadas por meio de semáforos ou mutexes.
+Nesta etapa você não precisa implementar as operações sobre arquivos listadas acima.
+Ao invés disso, use as operações diretamente do sistema de arquivos do seu sistema operacional.
+Recomenda-se que o servidor imprima mensagens na tela para demonstrar o funcionamento ao professor.
+
+Observações:
+Não é necessário autenticação dos usuários.
+Não é necessário criar um aplicativo cliente. Você pode usar o aplicativo netcat disponível para Linux e Windows.
+*/
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -10,124 +36,156 @@
 #include <time.h>
 #include <pthread.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 
 pthread_mutex_t mutex; // precisa ser global!
-
+/*
 char* listDir(){
 	char *retorno = calloc(2048,1);
 	int offset = 0;
 
-	DIR *d;
-    struct dirent *dir;
-    d = opendir(".");
+		DIR *d;
+	    struct dirent *dir;
+	    d = opendir(".");
 
-    if (d)
-    {
+    if (d){
         while ((dir = readdir(d)) != NULL)
-        {
             offset = offset + sprintf(retorno + offset, "%s ", dir->d_name);
-        }
+        
         closedir(d);
     }
 
     return retorno;
 }
 				
-void *func_thread(void *arg) {
+void *func_thread(void *arg){
 
 	pthread_mutex_lock(&mutex);
 	//região crı́tica
 	pthread_mutex_unlock(&mutex);
 	return 0;
+}*/
+
+void *ls(void *threadid){
+   	int *connfd = (int *)threadid;
+   	char sendBuff[1025];
+
+   	while (strcmp("exit\n", sendBuff) != 0){
+	   	int recebido = recv(*connfd, sendBuff, 1025, 0);
+		sendBuff[recebido] = '\0';
+		printf ("Valor recebido foi: %s", sendBuff);
+	   
+	   	if ((strncmp("mkdir ", sendBuff, 6)) == 0){
+			printf("\nESTOU NO MKDIR\n");
+			pthread_mutex_lock(&mutex);
+			system(sendBuff);
+			pthread_mutex_unlock(&mutex);
+		}
+
+		if ((strncmp("rm -r ", sendBuff, 6)) == 0){
+			printf("\nESTOU NO RM -R\n");
+			pthread_mutex_lock(&mutex);
+			system(sendBuff);		
+			pthread_mutex_unlock(&mutex);
+		}
+
+		if ((strncmp("cd ", sendBuff, 3)) == 0){
+			printf("\nESTOU NO CD\n");
+			pthread_mutex_lock(&mutex);
+			system(sendBuff);
+			pthread_mutex_unlock(&mutex);
+		}
+
+		if ((strncmp("ls", sendBuff, 2)) == 0){
+			printf("\nESTOU NO LS\n");
+			pthread_mutex_lock(&mutex);
+			system(sendBuff);
+			pthread_mutex_unlock(&mutex);
+		}
+
+		if ((strncmp("touch ", sendBuff, 6)) == 0){
+			printf("\nESTOU NO TOUCH\n");
+			pthread_mutex_lock(&mutex);
+			system(sendBuff);
+			pthread_mutex_unlock(&mutex);
+		}
+
+		if ((strncmp("rm ", sendBuff, 3)) == 0){
+			printf("\nESTOU NO RM\n");
+			pthread_mutex_lock(&mutex);
+			system(sendBuff);
+			pthread_mutex_unlock(&mutex);
+		}
+
+		if ((strncmp("echo ", sendBuff, 5)) == 0){			
+			printf("\nESTOU NO ECHO\n");
+			pthread_mutex_lock(&mutex);
+			system(sendBuff);
+			pthread_mutex_unlock(&mutex);
+		}
+
+		if ((strncmp("cat ", sendBuff, 4)) == 0){
+			printf("\nESTOU NO CAT\n");
+			pthread_mutex_lock(&mutex);
+			system(sendBuff);
+			pthread_mutex_unlock(&mutex);
+		}
+	}
 }
 
-void *print(void *threadid)
-{
-   int *connfd = (int *)threadid;
-   char sendBuff[1025];
+int main(int argc, char **argv){
+	int listenfd = 0;
+	struct sockaddr_in serv_addr;
+	char sendBuff[1025];
 
-   
-   printf("Socket na thread %d\n", *connfd);
+	listenfd = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-
-   int recebido = read(*connfd, sendBuff, 1025);
-   printf ("Valor recebido foi: %s", sendBuff);
-   
-   char msg[100];
-   sprintf(msg, "Recebi o valor %d bytes\n", recebido);
-
-   system("ls meuarquivo.txt > saida.txt");
-
-   char* ls = listDir();
-
-
-   write(*connfd, ls, 2048);
-   free(ls);
-   printf("Thread terminado\n");
-   close(*connfd); 	
-
-
-   free(connfd);
-   pthread_exit(NULL);
-};
-
-
-int main(int argc, char **argv)
-{
-
-		int listenfd = 0;
-		int message = 0;
-		struct sockaddr_in serv_addr;
-
+	memset(&serv_addr, '0', sizeof(serv_addr));
 		
-		//time_t ticks;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(5001);
 
+	bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
 
-		listenfd = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP);
-		memset(&serv_addr, '0', sizeof(serv_addr));
-		
+	listen(listenfd, 10);
 
-		serv_addr.sin_family = AF_INET;
-		serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-		serv_addr.sin_port = htons(5001);
+	while(1){
 
-		bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+		int *connfd = calloc(sizeof(int),1);
+		pthread_t thread;
 
-		listen(listenfd, 10);
-		
+		*connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+			
+		printf("Socket criado: %d\n", *connfd);
+		pthread_create(&thread, NULL, ls, connfd);
+			
+		//pthread_mutex_init(&mutex, NULL);
 
-		while(1)
+		/*if(sendBuff == "ls")
 		{
+			printf("cheguei ate AQUI\n");
+			pthread_create(&thread, NULL, ls, connfd);
+		/*} else if (strcmp(sendBuff, mkdir)== 0){
+		
+			pthread_create(&thread, NULL, mkdir, connfd);	
+		};
+		*/
+		
 
-			int *connfd = calloc(sizeof(int),1);
-			
-			pthread_t thread;
-
-
-			*connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-			
-
-				printf("Socket criado: %d\n", *connfd);
-
-				pthread_mutex_init(&mutex, NULL);
-
-				pthread_create(&thread, NULL, print, connfd);
-			
+		//case cwd:
+		//pthread_create(&thread, NULL, cwd, connfd);			
             
-//            snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
- //			write(connfd, sendBuff, strlen(sendBuff));
+		//snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+ 		//write(connfd, sendBuff, strlen(sendBuff));
 
 			//bzero(sendBuff, sizeof(sendBuff));
 
 
-            
-            
+		
+	}
 
-			//close(connfd);
-			//pthread_join(thread, NULL);
-			//pthread_mutex_destroy(&mutex);
-			//sleep(1);
-		}
-
+	//pthread_mutex_destroy(&mutex);
+	return 0;
 }
